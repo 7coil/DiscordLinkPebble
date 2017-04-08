22335fe80a4fb7305ccf0838647cd63123cea39b
7coil <leondrolio@gmail.com>
2017-04-08 23:11:22 +0100

    New DiscordJS branch

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,57 +1,19 @@
-var socket = require('./socket.io')('http://chat.infra.link');
+/* jshint esversion: 6 */
+
+var Discord = require("./discord.js/src/index");
+var client = new Discord.Client();
 
 Pebble.addEventListener('ready', function() {
 	
-	// PebbleKit JS is ready!
-	console.log('PebbleKit JS ready!');
-
-	printMessage("notify", "PebbleKit JS Initialised!");
-
-	socket.on("connect", function () {
-		socket.emit("user", {"username": "PebbleApp2"});
-	});
-
-	socket.on("system", function(data) {
-		printMessage(data.type, data.message);
-	});
-
-	socket.on("message", function(data) {
-		printMessage("message", data.username + "@" + data.source + ": " + data.message);
-	});
-
-	socket.on("url", function(data) {
-		printMessage("message", data.username + "@" + data.source + " sent an URL.");
-	});
-
-	Pebble.addEventListener('appmessage', function(e) {
-		var dict = e.payload;
-		console.log('Got message: ' + JSON.stringify(dict));
-		sendMessage("Pebble", "Pebble", dict.outmessage);
-	});
-
-});
-
-function printMessage(type, message) {
-	//Build the JSON to send to the pebble
-	var dict = {
-		'type': type,
-		'message': message
-	};
 	
-	console.dir(dict);
-
-	//Send message to the pebble
-	Pebble.sendAppMessage(dict, function() {
-		console.log('Message sent successfully: ' + JSON.stringify(dict));
-	}, function(e) {
-		console.log('Message failed: ' + JSON.stringify(e));
+	client.login("MTc4NTg2MDY5MzUxMTM3Mjgw.C6s7Rw.7X3gC_PyN0PLZbK5OCFmfmB3neg");
+	
+	client.on('ready', function() {
+		console.log("Discord.js Ready");
 	});
-}
-
-function sendMessage(source, username, message) {
-	socket.emit("message", {
-		source: source,
-		username: username,
-		message: message
+	
+	client.on('message', function(message) {
+		console.log(message.content);
 	});
-}
+	
+});
